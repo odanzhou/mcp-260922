9c2b09772c28093b6e4118e4420f81154454a62b
odanzhou <odanzhou@gmail.com>
2026-09-23 18:27:30 +0800

    feat: 处理打包

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
-export * from "./button";
-export * from "./card";
-export * from "./code";
+// 显式具名再导出：避免 CJS 产物里的 __exportStar 动态展开导致打包器无法静态识别导出
+export { ZButton } from "./button";
+export { Card } from "./card";
+export { Code } from "./code";
